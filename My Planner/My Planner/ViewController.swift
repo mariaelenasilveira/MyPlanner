b29405c5a44d2689c360c0a4ad337ec21860f043
Mariaelena Silveira <mariaelenaasilveira@gmail.com>
2021-06-08 20:25:13 -0300

    Inicializa e configura o projeto para uso do ViewCode

---
+++ My Planner/My Planner/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  My Planner
+//
+//  Created by Mariaelena Silveira on 08/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
