8ca89191cd59de5020b23a5f4c869e67691c2e1a
Mariaelena Silveira <mariaelenaasilveira@gmail.com>
2021-06-09 18:28:03 -0300

    Conclui configurações para uso do viewCode

--- My Planner/My Planner/ViewController.swift
+++ My Planner/My Planner/ViewController.swift
@@ -8,14 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var helloLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello World"
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.tintColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        setupView()
     }
 
+}
 
+extension ViewController: CodeView {
+    func setupAdditionalConfigurations() {
+        view.backgroundColor = .white
+    }
+    
+    func setupHierarchy() {
+        view.addSubview(helloLabel)
+    }
+    
+    func setupConstraints() {
+        helloLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        helloLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
 }
 
